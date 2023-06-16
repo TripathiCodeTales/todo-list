20dcc42a514785b227b7c62e3ca9503040111e17
TripathiCodeTales <133916225+TripathiCodeTales@users.noreply.github.com>
2023-06-16 14:38:54 +0530

    1 file modified, 1 added

---
+++ src/AddTask.js
@@ -0,0 +1,8 @@
+import react from 'reat';
+
+export default function AddTask (){
+  return(
+
+
+  )
+}
--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  function handleOnClick(){
+    
+  }
   return (
     <div>
       <h1>To Do List</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <button onClick = {handleOnClick}>AddTask</button>
+      
     </div>
   );
 }
